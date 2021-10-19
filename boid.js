49ce32c1eae958a1be1f8f10554037e9013af681
Charles NDAYISABA <nccharles1@gmail.com>
2021-10-20 00:56:44 +0200

    customizing

--- boid.js
+++ boid.js
@@ -1,94 +1,151 @@
+
 class Boid {
-  constructor() {
-    this.position = createVector(random(width), random(height));
-    this.velocity = p5.Vector.random2D();
-    this.velocity.setMag(random(2, 4));
-    this.acceleration = createVector();
-    this.maxForce = 0.2;
-    this.mxSpeed = 4;
-  }
+	constructor() {
+		this.position = createVector(random(width), random(height), random(depth));
+		
+		this.velocity = p5.Vector.random3D();
+		this.velocity.setMag(random(2, 4));
+		this.acceleration = createVector();
+		this.maxForce = 0.2;
+		this.maxSpeed = 10;
+		this.color = {r : random(10, 255), g : random(10, 255), b : random(10, 255)};
 
-  edges() {
-    if (this.position.x > width) {
-      this.position.x = 0;
-    } else if (this.position < 0) {
-      this.position.x = width;
-    }
-    if (this.position.y > height) {
-      this.position.y = 0;
-    } else if (this.position.y < 0) {
-      this.position.y = height;
-    }
-  }
-  align(boids) {
-    let perceptionRadius = 100;
-    let total = 0;
-    let steeling = createVector();
+		this.x = [],
+		this.y = [],
+		this.segNum = 20,
+		this.segLength = 4;
 
-    for (let other of boids) {
-      let d = dist(
-        this.position.x,
-        this.position.y,
-        other.position.x,
-        other.position.y
-      );
-      if (other !== this && d < perceptionRadius) {
-        steeling.add(other.position);
-        total++;
-      }
-    }
-    if (total > 0) {
-      steeling.div(total);
-      steeling.sub(this.position);
-      steeling.setMag(this.maxSpeed);
-      steeling.sub(this.velocity);
-      steeling.limit(this.maxForce);
-    }
-    return steeling;
-  }
-  cohesion(boids) {
-    let perceptionRadius = 100;
-    let total = 0;
-    let steeling = createVector();
+		for (var i = 0; i < this.segNum; i++) {
+			this.x[i] = 0;
+			this.y[i] = 0;
+		}
+	}
 
-    for (let other of boids) {
-      let d = dist(
-        this.position.x,
-        this.position.y,
-        other.position.x,
-        other.position.y
-      );
-      if (other !== this && d < perceptionRadius) {
-        steeling.add(other.position);
-        total++;
-      }
-    }
-    if (total > 0) {
-      steeling.div(total);
-      steeling.sub(this.position);
-      steeling.setMag(this.maxSpeed);
-      steeling.sub(this.velocity);
-      steeling.limit(this.maxForce);
-    }
-    return steeling;
-  }
-  flock(boids) {
-    this.acceleration.mult(0);
-    let cohesion = this.cohesion(boids);
-    let alignment = this.align(boids);
-    this.acceleration.add(alignment);
-    this.acceleration.add(cohesion);
-  }
-  update() {
-    this.position.add(this.velocity);
-    this.velocity.add(this.acceleration);
-    this.velocity.limit(this.maxSpeed);
-    this.acceleration.mult(0);
-  }
+	edges() {
+		if (this.position.x < 0 || this.position.x > width)
+			this.velocity.x = 0 - this.velocity.x;
+		
+		if (this.position.y < 0 || this.position.y > height)
+			this.velocity.y = 0 - this.velocity.y;
+		
+		if (this.position.z < 1 || this.position.z > depth)
+			this.velocity.z = 0 - this.velocity.z;
+		
+		return;
+		if (this.position.x > width) {
+			this.position.x = 0;
+		} else if (this.position.x < 0) {
+			this.position.x = width;
+		}
+		if (this.position.y > height) {
+			this.position.y = 0;
+		} else if (this.position.y < 0) {
+			this.position.y = height;
+		}
+	}
 
-  show() {
-    strokeWeight(8);
-    stroke(255);
-    point(this.position.x, this.position.y);
-  }
+	work(boids, perception) {
+		let align = createVector();
+		let cohesion = createVector();
+		let separation = createVector();
+		
+		let total = 0;
+		for (let other of boids) {
+			if (other != this) {
+				let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+				if (d < perception) {
+					align.add(other.velocity);
+
+					cohesion.add(other.position);
+
+					let diff = p5.Vector.sub(this.position, other.position);
+					diff.div(d * d);
+					separation.add(diff);
+
+
+
+					total++;
+				}
+			}
+		}
+		if (total > 0) {
+			align.div(total);
+			align.setMag(this.maxSpeed);
+			align.sub(this.velocity);
+			align.limit(this.maxForce);
+
+			cohesion.div(total);
+			cohesion.sub(this.position);
+			cohesion.setMag(this.maxSpeed);
+			cohesion.sub(this.velocity);
+			cohesion.limit(this.maxForce);
+
+			separation.div(total);
+			separation.setMag(this.maxSpeed);
+			separation.sub(this.velocity);
+			separation.limit(this.maxForce);
+		}
+		return {
+			align: align,
+			cohesion: cohesion,
+			separation: separation
+		};
+	}
+
+
+	flock(boids, perception) {
+		let w = this.work(boids, perception);
+		let alignment = w.align;
+		let cohesion = w.cohesion;
+		let separation = w.separation;
+
+		alignment.mult(alignSlider.value());
+		cohesion.mult(cohesionSlider.value());
+		separation.mult(separationSlider.value());
+
+		this.acceleration.mult(0);
+		this.acceleration.add(alignment);
+		this.acceleration.add(cohesion);
+		this.acceleration.add(separation);
+		this.acceleration.z *= 0.0001;
+	}
+
+	update() {
+		this.position.add(this.velocity);
+		this.velocity.add(this.acceleration);
+		this.velocity.limit(this.maxSpeed);
+	}
+
+	show() {
+
+		strokeWeight(this.position.z);
+		
+		this.dragSegment(0, this.position.x, this.position.y);
+		
+		for( var i= 0; i < this.x.length - 1; i++)
+			this.dragSegment(i + 1, this.x[i], this.y[i]);
+		
+		
+
+		strokeWeight(8);
+		stroke(this.color.r, this.color.g, this.color.b);
+		point(this.position.x, this.position.y);
+	}
+
+	dragSegment(i, xin, yin) {
+		var dx = xin - this.x[i];
+		var dy = yin - this.y[i];
+		var angle = atan2(dy, dx);
+		this.x[i] = xin - cos(angle) * this.segLength;
+		this.y[i] = yin - sin(angle) * this.segLength;
+		this.segment(this.x[i], this.y[i], angle);
+	  }
+	  
+	  segment(x, y, a) {
+		push();
+		translate(x, y);
+		rotate(a);
+		line(0, 0, this.segLength, 0);
+		pop();
+	  }
 }
--- sketch.js
+++ sketch.js
@@ -1,19 +1,41 @@
+
 const flock = [];
 
+let alignSlider, cohesionSlider, separationSlider;
+let depth = 8;
+
 function setup() {
-  createCanvas(1500, 800);
+  createCanvas(window.innerWidth, window.innerHeight - 30);
+  alignSlider = createSlider(0, 2, 1.5, 0.1);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  separationSlider = createSlider(0, 4, 2, 0.1);
+  perceptionSlider = createSlider(1, width > height ? height : width, 100, 1);
   for (let i = 0; i < 100; i++) {
     flock.push(new Boid());
   }
+
 }
 
 function draw() {
-  background(51);
-
+  background(0);
+	
+  let perception = perceptionSlider.value();	
   for (let boid of flock) {
-    boid.flock(flock);
+    boid.flock(flock, perception);
+    boid.edges();
     boid.update();
     boid.show();
-    boid.edges();
-  }
+  } 
+
+
+  textFont("Arial");
+  textSize(12);
+  fill(255);
+  stroke(0);
+
+  let size = 100;
+  text(alignSlider.value(), size * 1, height - 14);
+  text(cohesionSlider.value(), size * 2, height - 14);
+  text(separationSlider.value(), size * 3, height - 14);
+  text(perceptionSlider.value(), size * 4, height - 14);
 }
