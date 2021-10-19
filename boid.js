f73aa78e477a5affbbb0e4d32db22890d50800bb
Charles NDAYISABA <nccharles1@gmail.com>
2021-10-19 23:44:45 +0200

    working on align method

--- boid.js
+++ boid.js
@@ -1,16 +1,39 @@
-class Boid{
-    constructor(){
-        this.position=createVector(width/2,height/2);
-        this.velocity=p5.Vector.random2D();
-        this.acceleration=createVector();
+class Boid {
+  constructor() {
+    this.position = createVector(width / 2, height / 2);
+    this.velocity = p5.Vector.random2D();
+    this.acceleration = createVector();
+  }
+
+  align(boids) {
+    let perceptionRadius = 100;
+    let total = 0;
+    let avg = createVector();
+
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other !== this && d < perceptionRadius) {
+        avg.add(other.velocity);
+        total++;
+      }
     }
-update(){
+    if (total > 0) {
+      avg.div(boids.length);
+    }
+  }
+  update() {
     this.position.add(this.velocity);
     this.velocity.add(this.acceleration);
-}
-    show(){
-        strokeWeight(16);
-        stroke(255);
-        point(this.position.x, this.position.y)
-    }
+  }
+
+  show() {
+    strokeWeight(16);
+    stroke(255);
+    point(this.position.x, this.position.y);
+  }
 }
